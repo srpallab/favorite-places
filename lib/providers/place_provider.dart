9841cab7e8a45fae1588136b90e48167e31c0718
Shafiqur Rahman <pallab.alif@gmail.com>
2023-11-29 00:48:13 +0600

    ADD: SQL Lite database added.

--- lib/providers/place_provider.dart
+++ lib/providers/place_provider.dart
@@ -1,12 +1,78 @@
+import 'dart:developer';
+import 'dart:io';
+
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:path/path.dart' as path;
+import 'package:path_provider/path_provider.dart' as sys_paths;
+import 'package:sqflite/sqflite.dart' as sql;
+import 'package:sqflite/sqlite_api.dart';
 
 import '../models/place.dart';
 
 class UserPlacesNotifier extends StateNotifier<List<Place>> {
   UserPlacesNotifier() : super([]);
 
-  void addPlace(Place place) {
-    state = [...state, place];
+  Future<Database> _getDatabase() async {
+    final String dbPath = await sql.getDatabasesPath();
+    final Database db = await sql.openDatabase(
+      path.join(dbPath, 'places.db'),
+      onCreate: (db, version) {
+        return db.execute(
+          'CREATE TABLE user_places(id TEXT PRIMARY KEY, title TEXT,'
+          'image TEXT, lat REAL, lng REAL, address TEXT)',
+        );
+      },
+      version: 1,
+    );
+    return db;
+  }
+
+  Future<void> loadPlaces() async {
+    final db = await _getDatabase();
+    final data = await db.query("user_places");
+    final places = data
+        .map(
+          (row) => Place(
+            id: row['id'] as String,
+            title: row['title'] as String,
+            image: File(row['image'] as String),
+            location: PlaceLocation(
+              latitude: row['lat'] as double,
+              longitude: row['lng'] as double,
+              address: row['address'] as String,
+            ),
+          ),
+        )
+        .toList();
+    state = places;
+  }
+
+  Future<void> addPlace(Place place) async {
+    final appDir = await sys_paths.getApplicationDocumentsDirectory();
+    final fileName = path.basename(place.image.path);
+    final copiedImage = await place.image.copy(
+      '${appDir.path}/$fileName',
+    );
+
+    log(appDir.toString());
+
+    final newPlace = Place(
+      title: place.title,
+      image: copiedImage,
+      location: place.location,
+    );
+
+    final db = await _getDatabase();
+    db.insert("user_places", {
+      'id': newPlace.id,
+      'title': newPlace.title,
+      'image': newPlace.image.path,
+      'lat': newPlace.location.latitude,
+      'lng': newPlace.location.longitude,
+      'address': newPlace.location.address,
+    });
+
+    state = [newPlace, ...state];
   }
 }
 
--- lib/widgets/places_list_widget.dart
+++ lib/widgets/places_list_widget.dart
@@ -1,44 +1,75 @@
-import 'package:favorite_places/screens/place_details.dart';
+import 'package:favorite_places/providers/place_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-import '../models/place.dart';
-import '../providers/place_provider.dart';
+import '../screens/place_details.dart';
 
-class PlacesList extends ConsumerWidget {
+class PlacesList extends ConsumerStatefulWidget {
   const PlacesList({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    List<Place> placeList = ref.watch(userPlacesProvider);
-    return ListView.builder(
-      itemCount: placeList.length,
-      itemBuilder: (ctx, index) {
-        return Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: ListTile(
-            onTap: () {
-              Navigator.of(context).push(MaterialPageRoute(builder: (ctx) {
-                return PlaceDetailsScreen(place: placeList[index]);
-              }));
-            },
-            leading: CircleAvatar(
-              backgroundImage: FileImage(placeList[index].image),
-            ),
-            title: Text(
-              placeList[index].title,
-              style: Theme.of(context).textTheme.titleMedium!.copyWith(
-                    color: Theme.of(context).colorScheme.onBackground,
+  ConsumerState createState() => _PlacesListState();
+}
+
+class _PlacesListState extends ConsumerState<PlacesList> {
+  late Future<void> _placesFuture;
+
+  @override
+  void initState() {
+    super.initState();
+    _placesFuture = ref.read(userPlacesProvider.notifier).loadPlaces();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final placeList = ref.watch(userPlacesProvider);
+    return FutureBuilder(
+      future: _placesFuture,
+      builder: (BuildContext context, AsyncSnapshot snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(child: CircularProgressIndicator());
+        } else {
+          if (placeList.isEmpty) {
+            return const Center(
+              child: Text(
+                "No Data Found.",
+                style: TextStyle(color: Colors.white, fontSize: 26),
+              ),
+            );
+          } else {
+            return ListView.builder(
+              itemCount: placeList.length,
+              itemBuilder: (ctx, index) {
+                return Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ListTile(
+                    onTap: () {
+                      Navigator.of(context)
+                          .push(MaterialPageRoute(builder: (ctx) {
+                        return PlaceDetailsScreen(place: placeList[index]);
+                      }));
+                    },
+                    leading: CircleAvatar(
+                      backgroundImage: FileImage(placeList[index].image),
+                    ),
+                    title: Text(
+                      placeList[index].title,
+                      style: Theme.of(context).textTheme.titleMedium!.copyWith(
+                            color: Theme.of(context).colorScheme.onBackground,
+                          ),
+                    ),
+                    subtitle: Text(
+                      placeList[index].location.address,
+                      style: Theme.of(context).textTheme.bodySmall!.copyWith(
+                            color: Theme.of(context).colorScheme.onBackground,
+                          ),
+                    ),
                   ),
-            ),
-            subtitle: Text(
-              placeList[index].location.address,
-              style: Theme.of(context).textTheme.bodySmall!.copyWith(
-                    color: Theme.of(context).colorScheme.onBackground,
-                  ),
-            ),
-          ),
-        );
+                );
+              },
+            );
+          }
+        }
       },
     );
   }
